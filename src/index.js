b2558ffe43af2b8143d909dd427243c1f61801cd
Renee <156127959+reneexie01@users.noreply.github.com>
2024-07-30 06:40:02 +1000

    Create a new project and add a new task to the project via the console

--- src/index.js
+++ src/index.js
@@ -1,1 +1,77 @@
-console.log('webpack')
+const projectManager = (function ProjectManager() {
+    let projectsLibrary = [];
+
+    const renderProjects = () => console.log(projectsLibrary);
+
+    const createProject = function() {
+        let project = [];
+        projectsLibrary.push(project);
+    }
+
+    return { renderProjects, createProject, projectsLibrary };
+})();
+
+const addTask = (function AddTask() {
+
+    let taskCard = {};
+
+    const newTask = (task, description, due, priority, notes) => {
+        taskCard = {
+            'task' : task, 
+            'description' : description, 
+            'due' : due,
+            'priority' : priority,
+            'notes' : notes,
+        }
+        
+        return taskCard;
+    }
+
+    const addToProject = (project) => {
+        project.push(taskCard);
+    }
+
+    const clearTaskCard = (taskCard) => {
+        taskCard = {};
+    }
+
+    return { newTask, addToProject, clearTaskCard }
+})();
+
+console.log('empty project library: ');
+projectManager.renderProjects(); 
+
+console.log('creating project... ');
+projectManager.createProject();
+
+console.log('library with one project: ');
+projectManager.renderProjects();
+
+console.log('creating project 2... ');
+projectManager.createProject();
+
+console.log('library with two projects: ');
+projectManager.renderProjects();
+
+console.log('finding a specific project', projectManager.projectsLibrary[1]);
+
+console.log('making new task...');
+addTask.newTask('Odin Project', 'Finish course', '2020-01-01', 'High', 'Self-study');
+
+console.log('adding task to project[1]...');
+addTask.addToProject(projectManager.projectsLibrary[1]);
+
+console.log('library with two projects and one task in project[1]: ');
+projectManager.renderProjects();
+
+console.log('clearing task card...');
+addTask.clearTaskCard();
+
+console.log('making new task...');
+addTask.newTask('Odin Project2', 'Finish course2', '2020-01-012', 'High2', 'Self-study2');
+
+console.log('adding another task to project[1]...');
+addTask.addToProject(projectManager.projectsLibrary[1]);
+
+console.log('library with two projects and two tasks in project[1]: ');
+projectManager.renderProjects();
